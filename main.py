b6c8cba92308fe6d3a969a0886fd5d641123e5f6
bipms <95898947+bipms@users.noreply.github.com>
2022-01-16 20:43:04 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+
+
+st.sidebar.write("**Silahkan pilih nama UMKM/Klaster Anda**")
+st.sidebar.selectbox('Nama UMKM/Klaster',[1,2,3])
+st.sidebar.button('Pilih')
+
+
+option = st.sidebar.selectbox(
+     'How would you like to be contacted?',
+     ('Email', 'Home phone', 'Mobile phone'))
+st.sidebar.write('Produk yang dicatat di SIKEPANG:')
+st.sidebar.table('')
+
+
